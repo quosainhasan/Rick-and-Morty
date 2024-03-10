26710c252b4aa76454d18ee0f6ac2854f6c42114
Quosain Hasan <33932120+quosainhasan@users.noreply.github.com>
2024-03-10 11:55:53 +0000

    Update routes and views for character details

--- index.js
+++ index.js
@@ -7,12 +7,36 @@ const people = '/character';
 const locations = '/location';
 const episodes = '/episode';
 
-app.get('/', async(req, res) => {
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+});
+
+app.get('/characters', async(req, res) => {
     try{
         const page = req.query.page || 1;
         const characters = await axios.get(`${baseUrl}${people}?page=${page}`);
         const pages =  characters.data.info.pages;
         res.render('index.ejs', { characters: characters.data.results, pages: pages, page: page });
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
+app.get('/character/:id', async(req, res) => {
+    try{
+        const id = req.params.id;
+        const character = await axios.get(`${baseUrl}${people}/${id}`);
+        const episode = character.data.episode;
+        var nameofEpisodes = [];
+        for (let i = 0; i < episode.length; i++) {
+            const episodeData = await axios.get(episode[i]);
+            nameofEpisodes.push(episodeData.data);
+        }
+        res.render('character.ejs', { character: character.data, nameofEpisodes: nameofEpisodes });
     } catch (error) {
         console.error(error);
     }
